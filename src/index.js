0b3894a387a3532afc73b8de33ea793d35cd0414
SaatvatG <saatvatg@gmail.com>
2025-03-19 12:39:40 +0530

    added implementation for test case 1

--- src/index.js
+++ src/index.js
@@ -19,9 +19,10 @@ function calculateDistance(point1, point2) {
     diffLat = lat2 - lat1;
     diffLong = long2 - long1;
 
-    let insideRoot = Math.pow(Math.sin(diffLat / 2), 2) +
-                        Math.pow(Math.sin(diffLong / 2), 2) * Math.cos(lat1) * Math.cos(lat2);
-    
+    let insideRoot =
+        Math.pow(Math.sin(diffLat / 2), 2) +
+        Math.pow(Math.sin(diffLong / 2), 2) * Math.cos(lat1) * Math.cos(lat2);
+
     let ans = 2 * radius * Math.asin(Math.sqrt(insideRoot));
     return ans;
 }
@@ -36,33 +37,81 @@ function calculateDistance(point1, point2) {
  */
 function getRecommendedEvents(user, events, eventSimilarity, limit = 5) {
     // Your implementation here
+    let recommendations = [];
+    let preferencesExt = [];
+    let maxDistance = 0;
+    const candidateEvents = new Set();
 
-    let preferencesExt;
     // checking whether the user has any preferences
-    if (user.preferences.length > 0)
-        preferencesExt = [...user.preferences];
+    if (user.preferences.length > 0) preferencesExt = [...user.preferences];
 
     //checking whether the user has attended any events in the past
     if (user.attendedEvents.length > 0) {
-        user.attendedEvents.forEach(Id => {
-            const attendedEvent = events.find(event => event.id === Id);
-            //checking whether the attendedEvent has any other categories that
-            //can be added to preferences array
+        user.attendedEvents.forEach((Id) => {
+            // eventMap to be used here
+            const attendedEvent = events.find((event) => event.id === Id);
+            //checking whether the attendedEvents have any other categories
             if (attendedEvent && attendedEvent.categories) {
-                attendedEvent.categories.forEach(category => {
+                attendedEvent.categories.forEach((category) => {
                     if (!preferencesExt.includes(category)) {
                         preferencesExt.push(category);
                     }
                 });
             }
+            // finding the max distance that the user will go
+            if (attendedEvent && attendedEvent.location) {
+                const distance = calculateDistance(user.location, attendedEvent.location);
+                maxDistance = Math.max(distance, maxDistance);
+            }
+
+            if (eventSimilarity[Id]) {
+                eventSimilarity[Id].forEach((similarEventId) => {
+                    if (!user.attendedEvents.includes(similarEventId))
+                        candidateEvents.add(similarEventId);
+                });
+            }
         });
     }
+    events.forEach((event) => {
+        if (user.attendedEvents.includes(event.id))
+            return;
 
+        const distance = calculateDistance(user.location, event.location);
+        if (distance > maxDistance * 1.15) {
+            return;
+        }
 
+        let preferencePoints = 0;
+        if (preferencesExt.length > 0) {
+            if (event.categories) {
+                event.categories.forEach((category) => {
+                    if (preferencesExt.includes(category)) {
+                        preferencePoints += 1;
+                    }
+                });
+            }
+        }
 
+        const similarityPoints = candidateEvents.has(event.id) ? 1 : 0;
+        const distancePoints = 1 - distance / (maxDistance * 1.25);
+        
+        const totalPoints = preferencePoints * 0.4 + similarityPoints * 0.15 +
+                                distancePoints * 0.35 + event.popularity * 0.1;
+        
+        recommendations.push({
+            event,
+            distancePoints: distancePoints.toFixed(4),
+            similarityPoints: similarityPoints,
+            preferencePoints: preferencePoints,
+            totalPoints: totalPoints
+        });
+    });
+    recommendations.sort((a, b) => b.totalPoints - a.totalPoints);
+    // return recommendations.slice(0, limit);
+    return recommendations.slice(0, limit).map((item) => item.event);
 }
 
 module.exports = {
     calculateDistance,
-    getRecommendedEvents
+    getRecommendedEvents,
 };
